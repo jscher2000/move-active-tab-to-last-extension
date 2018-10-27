cd99aad224b79def0ef1b1d945e0792115a2b84e
Jefferson Scher <jscher2000@gmail.com>
2018-10-27 13:44:57 -0700

    v0.3
    Simplified after reading the documentation. Duh!

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 /* 
   Copyright 2018. Jefferson "jscher2000" Scher. License: MPL-2.0.
-  Revision 0.2 - 2018-10-27
+  Revision 0.3 - 2018-10-27
 */
 
 // Listen for tab activation and move the tab (if it is not pinned)
@@ -12,19 +12,11 @@ browser.tabs.onActivated.addListener((info) => {
 		if (currTab.pinned){
 			// Do nothing with pinned tabs
 		} else {
-			// (3) Find all tabs in the window
-			browser.tabs.query({windowId: info.windowId}).then((foundtabs) => {
-				// (4) Compute the index of the last tab in the window 
-				// (it's an array, so *assume* the number of items minus 1 is the highest possible index)
-				var maxindex = foundtabs.length - 1;
-				// (5) Move the current tab to the end if we're not there already
-				if (info.tabId !== maxindex){
-					browser.tabs.move(info.tabId, {index: maxindex}).catch((err) => {
-						console.log('tabs.move() error: ' + err.message);
-					});
-				}
-			}).catch((err) => {
-				console.log('tabs.query() error: ' + err.message);
+			// (3) Move the current tab to the end
+			// for index: "A value of -1 will place the tab at the end of the window."
+			// https://developer.mozilla.org/docs/Mozilla/Add-ons/WebExtensions/API/tabs/move
+			browser.tabs.move(info.tabId, {index: -1}).catch((err) => {
+				console.log('tabs.move() error: ' + err.message);
 			});
 		}
 	}).catch((err) => {
